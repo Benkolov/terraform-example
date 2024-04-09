c6fc1b65d2c33f62fec5638628e219e2adca5e57
Boyan Benkolov <bbenkolov@gmail.com>
2024-04-09 20:25:37 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -64,7 +64,6 @@ resource "azurerm_mssql_database" "database" {
   collation      = "SQL_Latin1_General_CP1_CI_AS"
   license_type   = "LicenseIncluded"
   max_size_gb    = 1
-  
   sku_name       = "Basic"
   zone_redundant = false
 }
